e7feb555efa9b49a29a5fccb187a9c553bbc0344
MaikaSaba <saba.maika@gmail.com>
2024-03-05 20:00:19 -0400

    index.js created, first question working, generateMarkdown not working

---
+++ index.js
@@ -0,0 +1,42 @@
+//Packages 
+//Inquirer npm 
+const inquirer = require("inquirer");
+//fs built in node
+const fs = require('fs');
+// generateMarkdown.js file
+const generateMarkdown = require('./utils/generateMarkdown')
+
+//Array of questions
+const questions = [{
+    type: 'input',
+    message: "Please enter the title of the project.",
+    name: "title",
+},
+];
+
+//Write README file function
+function writeInFile(data) {
+    // variable for readme name
+    const fileName = `${data.title.split(" ").join("")}.README.md`;
+    //variable for generateMarkdown
+    const dataString = generateMarkdown(data);
+    //Write readme with parameters, fileName, dataString and err
+    fs.writeFile(fileName, dataString, (err) =>{
+        //if error and success
+        err ? console.log(err) : console.log("README Successufuly Created!")
+     });
+};
+
+//Initialize app function
+function init() {
+    inquirer
+    //show questions in console
+    .prompt(questions)
+    //run writeInFile
+    .then((data) => {
+        writeInFile(data);
+    });
+};
+
+//call to initialize
+init()
